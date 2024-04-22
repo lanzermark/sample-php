e737a1f8a5995e566c714aadb54f679bc2e4f386
lanzermark <167761162+lanzermark@users.noreply.github.com>
2024-04-22 17:27:49 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,15 @@
 <?php
-require __DIR__ . '/vendor/autoload.php';
+// require __DIR__ . '/vendor/autoload.php';
 
-use Cowsayphp\Farm;
+// use Cowsayphp\Farm;
 
-header('Content-Type: text/plain');
+// header('Content-Type: text/plain');
 
-$text = "Set a message by adding ?message=<message here> to the URL";
-if(isset($_GET['message']) && $_GET['message'] != '') {
-	$text = htmlspecialchars($_GET['message']);
-}
+// $text = "Set a message by adding ?message=<message here> to the URL";
+// if(isset($_GET['message']) && $_GET['message'] != '') {
+// 	$text = htmlspecialchars($_GET['message']);
+// }
 
-$cow = Farm::create(\Cowsayphp\Farm\Cow::class);
-echo $cow->say($text);
+// $cow = Farm::create(\Cowsayphp\Farm\Cow::class);
+// echo $cow->say($text);
+echo "Welcome to my seller app";
